f399fffe3aa1e4391ee4684151a3cc41fe50ab8d
JynLeazy <80413930+JynLeazy@users.noreply.github.com>
2021-06-03 10:24:41 -0500

    add } at end jenkinsfile.groovy

--- caller/jenkinsfile.groovy
+++ caller/jenkinsfile.groovy
@@ -8,10 +8,12 @@ node { //allocates a Jenkins executor and the workspace for the Pipeline
       responder = load 'responder/responder.groovy' //assign the second groovy file to the defined variable with the command "load"
       println responder //prints the variable
       responder.hello('hi') // loads the second groovy file and executes the function "hello" sending as a parameter the value "hi"
-    } catch (Exception err){ //using try/catch structure to get the errors
+    } 
+    catch (Exception err){ //using try/catch structure to get the errors
         println err
       }
   }
 }
 def repoclone(){ //defines a function to clone the repositories to the Jenkins' workspace
-checkout([$class: 'GitSCM', branches: [[name: '*/'+'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory'], [$class: 'MessageExclusion', excludeMessage: '(?s).*JENKINS_IGNORE.*']], submoduleCfg: [], userRemoteConfigs: [[url:"https://github.com/JynLeazy/WorkingWithGroovyFiles.git"]]])
+  checkout([$class: 'GitSCM', branches: [[name: '*/'+'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory'], [$class: 'MessageExclusion', excludeMessage: '(?s).*JENKINS_IGNORE.*']], submoduleCfg: [], userRemoteConfigs: [[url:"https://github.com/JynLeazy/WorkingWithGroovyFiles.git"]]])
+}
